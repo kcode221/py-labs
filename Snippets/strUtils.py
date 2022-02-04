703e17bcb7d06bf75897e5c1459794c6774f9384
kcode221 <kira.jow@gmail.com>
2022-02-04 18:01:58 +0000

    Pig latin

--- Snippets/strUtils.py
+++ Snippets/strUtils.py
@@ -1,6 +1,12 @@
+
 def str_reverse(s):
     return s[::-1]
 
 def check_palindrome(s):
     return s == s[::-1]
 
+#pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+#pig_it('Hello world !')     # elloHay orldway !
+
+def pig_it(text):
+    return (" ").join(map(lambda x: x[1:]+x[0]+'ay' if x.isalpha() else x, text.split(" ")))
--- Snippets/testUtils.py
+++ Snippets/testUtils.py
@@ -5,3 +5,6 @@ print(m.sub(2,3))
 
 print(s.str_reverse('hello world'))
 print(s.check_palindrome('abcdcba'))
+
+print(s.pig_it('Pig latin is cool')) # igPay atinlay siay oolcay
+print(s.pig_it('Hello world !'))    # elloHay orldway !
